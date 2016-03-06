13f42ea1a72fa72ed08cc389ba85fb747701b702
vincent <vincent314@hotmail.fr>
2016-03-06 02:32:13 +0100

    feat(slides): add reveal slides

--- back/step5.correction.spec.ts
+++ back/step5.correction.spec.ts
@@ -1,17 +1,17 @@
-var model = require('./model');
+import {Person,Contract} from "./model";
 
-describe('Step 5',()=>{
-   it('Test module',()=>{
-       var owner = new model.Person('Doe','John');
-       var contract = new model.Contract('0123456', owner);
+describe('Step 5', ()=> {
+    it('Test module', ()=> {
+        var owner = new Person('Doe', 'John');
+        var contract = new Contract('0123456', owner);
 
-       expect(contract).toEqual({
-           id:'0123456',
-           owner:{
-               name:'Doe',
-               surname:'John'
-           }
-       });
+        expect(contract).toEqual({
+            id: '0123456',
+            owner: {
+                name: 'Doe',
+                surname: 'John'
+            }
+        });
 
-   });
+    });
 });
